50cc1820d10e814d8c7a3afb2afa0f235164dec7
StefanV <s_v112@yahoo.de>
2023-09-15 13:25:23 +0000

    start stop

--- initServer/init_server.sh
+++ initServer/init_server.sh
@@ -1,16 +1,19 @@
 OSRELEASE=`cat /etc/os-release |grep -i rhel`
 if [ "z" = "z$OSRELEASE" ] ; then
+  PACKETMAN="apt-get"
   echo "running apt-get for ubuntu"
   sudo apt-get -y update
   sudo apt-get -y upgrade
   echo "installung git "
   sudo apt-get install -y git
 else
+  PACKETMAN="yum"
   echo "running yum for Red Hat"
   sudo yum -y update 
   sudo yum -y upgrade 
   echo "installing git "
   sudo yum install -y git 
+  sudo yum install gcc openssl-devel bzip2-devel libffi-devel zlib-devel 
 fi
 
 git config --global user.email s_v112@yahoo.de
