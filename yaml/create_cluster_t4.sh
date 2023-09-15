207e92e004d5086f86c630b7ad685bbdf6cac46d
StefanV <s_v112@yahoo.de>
2023-09-15 09:34:31 +0000

    Chnage to deployment yaml

--- yaml/create_cluster_t4.sh
+++ yaml/create_cluster_t4.sh
@@ -1,4 +1,4 @@
 #eksctl create cluster --name fleetman --instance-selector-vcpus=1 --instance-selector-memory=1 --nodes-min 3 --version=1.27
 echo "Generating small cluster using 1 vCPU and 1 G memory"
-eksctl create cluster -f  gen-cluster.yaml
+eksctl create cluster -f  gen-cluster_4_cpu.yaml
 echo "Done"
