15cc34536cf1b1f1f2170aebe36befc80df8c7db
StefanV <s_v112@yahoo.de>
2023-09-15 10:03:14 +0000

    adding init and k8s start/stop script

--- initServer/init_server.sh
+++ initServer/init_server.sh
@@ -3,8 +3,19 @@ if [ "z" = "z$OSRELEASE" ] ; then
   echo "running apt-get for ubuntu"
   sudo apt-get -y update
   sudo apt-get -y upgrade
+  echo "installung git "
+  sudo apt-get install -y git
 else
   echo "running yum for Red Hat"
   sudo yum -y update 
   sudo yum -y upgrade 
+  echo "installing git "
+  sudo yum install -y git 
 fi
+
+git config --global user.email s_v112@yahoo.de
+git config --global user.name "StefanV"
+
+mkdir ~/git 
+cd ~/git 
+
