d1df28a9fe0c35d088f42071f976ad09766c1409
StefanV <s_v112@yahoo.de>
2023-09-15 09:12:09 +0000

    init scriüt reviewd

--- initServer/init_aws.sh
+++ initServer/init_aws.sh
@@ -3,6 +3,9 @@ mkdir Downloads
 cd Downloads
 
 echo "Install EKS"
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp 
@@ -16,7 +19,9 @@ echo "Install  kubernetes"
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.4/2023-08-16/bin/linux/amd64/kubectl
 
 chmod +x kubectl 
-mv kubectl /usr/local/bin 
+sudo mv kubectl /usr/local/bin 
+
+kubectl version 
 
 echo "install AWS 2"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -24,5 +29,9 @@ unzip awscliv2.zip
 
 sudo ./aws/install
 aws --version
+
+rm -R  aws
+rm awscliv2.zip 
 /usr/local/bin/aws --version
 
+echo "sucessfully "
--- initServer/init_server.sh
+++ initServer/init_server.sh
@@ -1,2 +1,10 @@
-sudo apt-get -y update
-sudo apt-get -y upgrade
+OSRELEASE=`cat /etc/os-release |grep -i rhel`
+if [ "z" = "z$OSRELEASE" ] ; then
+  echo "running apt-get for ubuntu"
+  sudo apt-get -y update
+  sudo apt-get -y upgrade
+else
+  echo "running yum for Red Hat"
+  sudo yum -y update 
+  sudo yum -y upgrade 
+fi
